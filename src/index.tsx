39fb8d10ef8aadb7454ee7fd34d7c257020315c3
Matheus Velloso <matheustvelloso@gmail.com>
2025-06-13 02:42:17 -0300

    chore: remove apollo client

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React, { Suspense } from 'react';
 
-import { ApolloProvider } from '@apollo/client';
 import ReactDOM from 'react-dom/client';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -8,8 +7,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'services/i18n';
 
 import { ThemeProvider } from 'styled-components';
-
-import PokemonClient from 'services/PokemonClient';
 
 import GlobalStyles from 'styles/GlobalStyles';
 
@@ -20,12 +17,10 @@ import App from './App';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Suspense>
-      <ApolloProvider client={PokemonClient}>
-        <ThemeProvider theme={styledComponentsTheme}>
-          <App />
-          <GlobalStyles />
-        </ThemeProvider>
-      </ApolloProvider>
+      <ThemeProvider theme={styledComponentsTheme}>
+        <App />
+        <GlobalStyles />
+      </ThemeProvider>
     </Suspense>
   </React.StrictMode>,
 );
